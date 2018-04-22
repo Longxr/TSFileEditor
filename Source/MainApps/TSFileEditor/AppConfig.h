927d59c77272410ec049c9e94006c4659710585c
longxr <longxuanren@gmail.com>
2018-04-22 11:04:45 +0800

    add ignore file

--- Source/MainApps/TSFileEditor/AppConfig.h
+++ Source/MainApps/TSFileEditor/AppConfig.h
@@ -1,7 +1,7 @@
 #ifndef APPCONFIG_H
 #define APPCONFIG_H
 
-#define APP_KEY     "应用ID"
-#define APP_SECRET  "应用密钥"
+#define APP_KEY     "149a78d6b76617a0"
+#define APP_SECRET  "Fvzk5zpwedizqX97So4yZoe3wrQBTpAN"
 
 #endif // APPCONFIG_H
