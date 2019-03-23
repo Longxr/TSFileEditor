596c6532c001b2397769bdd17239e4706749924d
xuanren.long <xuanren.long@mythware.com>
2019-03-23 10:20:05 +0800

    add AppConfig.h

---
+++ Source/MainApps/TSFileEditor/AppConfig.h
@@ -0,0 +1,7 @@
+#ifndef APPCONFIG_H
+#define APPCONFIG_H
+
+#define APP_KEY         "https://ai.youdao.com get key"
+#define APP_SECRET      "https://ai.youdao.com get secret"
+
+#endif // APPCONFIG_H
